62c4520edb8b627012e28358c66a45c2d151082b
Query Kuma <querykuma@users.noreply.github.com>
2021-02-02 03:27:47 +0900

    fix: yahoo updated

--- autopagerize_yahoo_comment-bookmarklet.js
+++ autopagerize_yahoo_comment-bookmarklet.js
@@ -6,10 +6,11 @@ javascript: (() => {
     const sleep_time = 1000;
     const insertFrame = (times, page, baseNode) => {
         if (times <= 0) return;
-        const frameURL = `https://news.yahoo.co.jp/comment/plugin/v1/full/?origin=https://headlines.yahoo.co.jp&sort=${baseNode.getAttribute('data-sort')}&order=${baseNode.getAttribute('data-order')}&page=${page}&type=t&keys=${baseNode.getAttribute('data-keys')}&full_page_url=${baseNode.getAttribute('data-full-page-url')}&comment_num=${baseNode.getAttribute('data-comment-num')}`;
+        const frameURL = `https://news.yahoo.co.jp/comment/plugin/v1/full/?origin=https://news.yahoo.co.jp&sort=lost_points&order=desc&page=${page}&type=t&topic_id=${baseNode.dataset.topicId}&space_id=${baseNode.dataset.spaceId}&content_id={baseNode.dataset.content_id}&full_page_url=${baseNode.dataset.fullPageUrl}&comment_num=${baseNode.dataset.commentNum}&flt=${baseNode.dataset.flt}&mtestid=${baseNode.dataset.mtestid}`;
         const frameNew = `<iframe class="news-comment-plguin-iframe" scrolling="yes" frameborder="0" src="${frameURL}" style="width: 100%; height: ${frame_height}; border: none;"></iframe>`;
+        console.log(frameURL);
         baseNode.insertAdjacentHTML('beforeend', frameNew);
-        setTimeout(() => { insertFrame(times - 1, page + 1, baseNode) }, sleep_time);
+        setTimeout(() => { insertFrame(times - 1, page + 1, baseNode); }, sleep_time);
     };
     const replaceFrame = () => {
         const iframes = document.querySelectorAll("iframe.news-comment-plguin-iframe");
@@ -20,4 +21,4 @@ javascript: (() => {
         insertFrame(page_times, start_page, baseNode);
     };
     replaceFrame();
-})()
+})();
